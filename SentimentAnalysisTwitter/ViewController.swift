4344835fe1e2a8c5b1e83cbdfcd0662d6d6605e1
fatimbow <fatmabasaran217@gmail.com>
2023-07-19 14:21:20 +0300

    App design completed.

--- SentimentAnalysisTwitter/ViewController.swift
+++ SentimentAnalysisTwitter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predictButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
