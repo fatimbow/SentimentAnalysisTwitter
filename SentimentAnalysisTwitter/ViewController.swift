c89849bd8fdeb93fae01ffa1927859189e673d24
fatimbow <fatmabasaran217@gmail.com>
2023-07-27 14:32:14 +0300

    Swifter added.

--- SentimentAnalysisTwitter/ViewController.swift
+++ SentimentAnalysisTwitter/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SwifteriOS
+
+var apiKey: String {
+  get {
+    guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+      fatalError("Couldn't find file 'Secrets.plist'.")
+    }
+    let plist = NSDictionary(contentsOfFile: filePath)
+    guard let value = plist?.object(forKey: "API_Key") as? String else {
+      fatalError("Couldn't find key 'API_Key' in 'Secrets.plist'.")
+    }
+    return value
+  }
+}
+
+var apiSecret: String {
+  get {
+    guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+      fatalError("Couldn't find file 'Secrets.plist'.")
+    }
+    let plist = NSDictionary(contentsOfFile: filePath)
+    guard let value = plist?.object(forKey: "API_Secret") as? String else {
+      fatalError("Couldn't find key 'API_Secret' in 'Secrets.plist'.")
+    }
+    return value
+  }
+}
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let swifter = Swifter(consumerKey: apiKey , consumerSecret: apiSecret)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+
+        
     }
 
     @IBAction func predictButton(_ sender: UIButton) {
